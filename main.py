9ec3ec5c446367485543c1acb2f2fa19de2f96f4
Valeriy Gorshevskiy <gorshchevskiy90@mail.ru>
2024-07-06 23:01:20 +0300

    punkt_2

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import os
-from utils import get_vacancies, create_database, save_data_to_database, get_employer, getEmployers
+from utils import create_database, save_data_to_database, get_employer, getEmployers, get_hh_ru_data
 from config import config
 
 def main():
-    api_hh_ru = 'https://api.hh.ru/vacancies'
+
     company_ids = [
-        '1473866',
-        '205',
+        '4307',
+        '4787018',
         '4219',
         '23040',
-        '740',
-        '3838',
+        '1579449',
+        '2180',
         '1057',
         '1180',
         '208707',
-        '1740'
+        '205'
     ]
 
     params = config()
     #employers = getEmployers()
-    data = get_vacancies(205)
-    date_2 = get_employer(205)
-   # print(get_vacancies(3421))
+
+    data = get_hh_ru_data(company_ids)
+    #print(data)
+    date_2 = get_employer(2180)
+    print(date_2)
    # create_database('hh_data', params)
    # save_data_to_database(data, 'hh_data', params)
 
--- utils.py
+++ utils.py
@@ -4,14 +4,54 @@ from typing import Any
 from googleapiclient.discovery import build
 import time
 import json
+from typing import List, Dict, Any
 
-'''def get_vacancies(api_hh_ru: str, company_ids: list[str]) -> list[dict[str, Any]]:
-    hh = build('hh', 'v3', developerKey=api_hh_ru)
 
+def get_hh_ru_data(company_ids: List[str]) -> List[Dict[str, Any]]:
+    """
+        Получение данных о работодателях и вакансиях с использованием API HH.ru.
+
+        Параметры:
+        - company_ids: Список строк, представляющих идентификаторы работодателей, для которых нужно получить данные.
+
+        Возвращает:
+        Список из словарей, (employers и vacancies) где каждый словарь содержит информацию о работодателе и список вакансий.
+        """
+    params = {
+        'area': 1,
+        'page': 0,
+        'per_page': 10
+    }
     data = []
-    for company_id in company_ids:
-        company_data = hh.companies().list(part='snippet, statistics',id=company_id).execute()
-        print(company_data)'''
+
+    for employer_id in company_ids:
+        url_emp = f"https://api.hh.ru/vacancies?employer_id={employer_id}"
+        employer_info = requests.get(url_emp, params=params).json()
+        #data.append(employer_info)
+        #print(data)
+        vacancies = []
+        url_vac = f"https://api.hh.ru/vacancies?employer_id={employer_id}"
+        vacancies_info = requests.get(url_vac, params=params).json()
+        vacancies.extend(vacancies_info['items'])
+        data.append({
+            'employers': employer_info,
+            'vacancies': vacancies
+        })
+    return data
+
+
+def get_employer(employer_id):
+    """Получаем данные о работодателях по API"""
+
+    url = f"https://api.hh.ru/employers/{employer_id}"
+    data_vacancies = requests.get(url).json()
+    hh_company = {
+        "employer_id": int(employer_id),
+        "company_name": data_vacancies['name'],
+        "open_vacancies": data_vacancies['open_vacancies']
+    }
+
+    return hh_company
 
 
 def getEmployers():
@@ -42,33 +82,6 @@ def getEmployers():
 #employers = getEmployers()
 
 
-def get_vacancies(employer_id):
-    """Получение данных вакансий по API"""
-
-    params = {
-        'area': 1,
-        'page': 0,
-        'per_page': 10
-    }
-    url = f"https://api.hh.ru/vacancies?employer_id={employer_id}"
-    data_vacancies = requests.get(url, params=params).json()
-    #print(data_vacancies)
-    vacancies_data = []
-    for item in data_vacancies["items"]:
-        hh_vacancies = {
-            'vacancy_id': int(item['id']),
-            'vacancies_name': item['name'],
-            'payment': item["salary"]["from"] if item["salary"] else None,
-            'requirement': item['snippet']['requirement'],
-            'vacancies_url': item['alternate_url'],
-            'employer_id': employer_id
-        }
-        #if hh_vacancies['payment'] is not None:
-        vacancies_data.append(hh_vacancies)
-
-        #return vacancies_data
-    print(vacancies_data)
-#print(get_vacancies(205)))'''
 
 
 def create_database(database_name: str, params: dict) -> None:
@@ -85,16 +98,12 @@ def save_data_to_database(data: list[dict[str, Any]], database_name: str, params
         :param params: (dict) параметры подключения к базе данных"""
 
 
-def get_employer(employer_id):
-    """Получаем данные о работодателях по API"""
 
-    url = f"https://api.hh.ru/employers/{employer_id}"
-    data_vacancies = requests.get(url).json()
-    hh_company = {
-        "employer_id": int(employer_id),
-        "company_name": data_vacancies['name'],
-        "open_vacancies": data_vacancies['open_vacancies']
-    }
-    #print(data_vacancies)
-    #return hh_company
-    print(hh_company)
+
+
+
+
+
+
+
+
